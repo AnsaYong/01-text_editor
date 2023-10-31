5fe4aab87cab240fae6fd9a4991d7045ff16bd02
AnsaYong <ansahmbomyong@gmail.com>
2023-10-31 14:18:28 +0200

    adding a header file and a file containing the entry point tot the program

---
+++ editor.c
@@ -0,0 +1,13 @@
+#include "editor.h"
+
+/**
+ * main - the main entry point of the program
+ * 
+ * Return: 0
+*/
+int main(void)
+{
+    printf("This is a simple text editor\n");
+
+    return (0);
+}
---
+++ editor.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE 1024
+
+/**
+ * struct line_s - stores lines from a file
+ * @line: character array to store chars from a line of text
+ * @next: pointer to the next node
+ * @prev: pointer to the previous node
+*/
+typedef struct line_s
+{
+    char line[MAX_LINE];
+    struct line_s *next;
+    struct line_s *prev;
+} line_t;
