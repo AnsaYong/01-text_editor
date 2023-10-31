56c1e0c3dfd599d8fd085d454fc437aeefe84a2a
AnsaYong <ansahmbomyong@gmail.com>
2023-10-31 14:40:50 +0200

    removing whitespaces from editor.c

--- editor.c
+++ editor.c
@@ -2,12 +2,12 @@
 
 /**
  * main - the main entry point of the program
- * 
+ *
  * Return: 0
 */
 int main(void)
 {
-    printf("This is a simple text editor\n");
+	printf("This is a simple text editor\n");
 
-    return (0);
+	return (0);
 }
